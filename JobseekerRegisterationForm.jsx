c4e2075c86b73cbb5f2f0cd62ee461dca55152ae
yosiefberhe <70027040+yosiefberhe@users.noreply.github.com>
2021-09-20 21:22:28 -0400

    Add files via upload

---
+++ JobseekerRegisterationForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+function JobseekerRegisterationScreen(props) {
+  return (
+    
+     <Text> Hello, this is Jobseeker form. FROM JOBSEEKER COMPONENT </Text>
+    
+  );
+}
+
+export default JobseekerRegisterationScreen;
